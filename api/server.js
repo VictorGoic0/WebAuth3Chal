2aeb3a3ca1b56359ab58949a35617c000fa53ffe
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-03 16:18:30 -0400

    Created generateToken function, imported secret at top from process.env, successful login will generate a token and send it back

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
 const authRouter = require("../routers/authRouter.js");
+const userRouter = require("../routers/userRouter.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use("/auth", authRouter);
+server.use("/api/users", userRouter);
 
 module.exports = server;
