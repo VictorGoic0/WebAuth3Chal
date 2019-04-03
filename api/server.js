5967b3c0ee7ad99ad98d85f5ee96af92940cb88a
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-03 15:56:57 -0400

    authRouter file created. Faker package added. Migration for users db and table made and ran, seed for mock data ran.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
+const authRouter = require("../routers/authRouter.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use("/auth", authRouter);
 
 module.exports = server;
