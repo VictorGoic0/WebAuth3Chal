e04e9f098057a59ea7f1636f45f3d56368255d12
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-03 16:00:30 -0400

    dbConfig added, helpers for users added

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -7,7 +7,7 @@ const createFakeUser = () => ({
   password: faker.internet.password()
 });
 
-exports.seed = async function(knex, Promise) {
+exports.seed = function(knex, Promise) {
   const fakeUsers = [];
 
   for (let i = 0; i < 100; i++) {
@@ -21,5 +21,5 @@ exports.seed = async function(knex, Promise) {
     user.department = "placeholder department";
   });
 
-  return await knex("users").insert(fakeUsers);
+  return knex("users").insert(fakeUsers);
 };
